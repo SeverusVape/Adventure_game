f94c141571991fe1e5f8e8b62e97d8990d24a8e2
Veter v Golove <koshelevworkmail@gmail.com>
2023-04-30 17:10:20 -0400

    Particles: enemy death

--- code/level.py
+++ code/level.py
@@ -87,7 +87,8 @@ class Level:
                                     (x, y),
                                     [self.visible_sprites, self.attackable_sprites],
                                     self.obstacle_sprites,
-                                    self.damage_player
+                                    self.damage_player,
+                                    self.trigger_death_particles
                                 )
 
     def create_attack(self):
@@ -124,6 +125,9 @@ class Level:
             self.player.vulnerable = False
             self.player.hurt_time = pygame.time.get_ticks()
             self.animation_player.create_particles(attack_type, self.player.rect.center, [self.visible_sprites])
+
+    def trigger_death_particles(self, pos, particle_type):
+        self.animation_player.create_particles(particle_type, pos, self.visible_sprites)
 
     def run(self):
         # update and draw the game
